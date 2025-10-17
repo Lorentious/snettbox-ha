f095f6398409a2a545d69cb4bd9335394a886c17
ThoreKystervicz <131364332+ThoreKystervicz@users.noreply.github.com>
2025-10-17 11:32:36 +0200

    T

--- custom_components/snettbox-ha/__init__.py
+++ custom_components/snettbox-ha/__init__.py
@@ -10,11 +10,11 @@ async def async_setup_entry(hass, entry):
     }
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
 async def async_unload_entry(hass, entry):
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    await hass.config_entries.async_forward_entry_unload(entry, ["sensor"])
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
